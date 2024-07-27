b6a8cbe1715675c694e2d90e1ea685d5d3c4c9cb
vinayak-urs <singhvinayakurs@gmail.com>
2024-07-27 20:11:11 +0530

    css fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const CustomNavbar = () => (
 transition-colors duration-500 lg:z-50
 lg:border-slate-900/10 dark:border-slate-50/[0.06]      
 md:flex-shrink-0 md:mx-0 md:my-0 md:py-4 md:px-[2vw] 
-sm:px-0 md:px-[10vw] 2xl:px-[20vw] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent"
+sm:px-0 md:px-[10vw] 2xl: bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent"
   >
     <Navbar />
   </aside>
@@ -16,7 +16,7 @@ sm:px-0 md:px-[10vw] 2xl:px-[20vw] bg-white/95 supports-backdrop-blur:bg-white/6
 
 function App() {
   return (
-    <div className="flex-auto pt-12 min-w-0 md:mt-0 flex bg-transparent flex-col md:pt-12 mx-[6vw] md:mx-[10vw] 2xl:mx-[20vw]  dark:bg-[#1f2028] bg-white antialiased min-h-screen flex flex-col transition-all duration-900 bg-white dark:bg-[#1f2028] min-h-lvh">
+    <div className="flex-auto pt-12 min-w-0 md:mt-0 flex bg-transparent flex-col md:pt-12 px-[6vw] md:px-[10vw] 2xl:px-[20vw]  dark:bg-[#1f2028] bg-white antialiased min-h-screen flex flex-col transition-all duration-900 bg-white dark:bg-[#1f2028] min-h-lvh">
       <CustomNavbar />
       <HomeSection />
       <CustomCursor />
